db260b8101a5acb2ee2c6d7a9c0f3534460840f5
sinbad <sinbad@sinbads-Mac.local>
2018-02-17 10:15:34 -0800

    - auto layout - custom mathod for auto layout

--- FacebookUI/ViewController.swift
+++ FacebookUI/ViewController.swift
@@ -8,16 +8,103 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+let cellId = "cellId"
+class FeedController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.view.backgroundColor = UIColor.red
+        navigationItem.title = "FaceBook Feed"
+        
+        collectionView?.backgroundColor = UIColor(white: 0.95, alpha: 1)
+        collectionView?.register(FeedCell.self, forCellWithReuseIdentifier: cellId )
     }
-
-  
-
-
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 60)
+    }
+    
+    
 }
+
+class FeedCell : UICollectionViewCell {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupView() {
+        backgroundColor = UIColor.white
+        
+        addSubview(NameLabel)
+        addSubview(ProfileImageView)
+        
+       // addConstraisWithFormat(format:  "H:|-8-[v0(44)]-8-[v1]|", views: ProfileImageView, NameLabel)
+        
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-8-[v0(44)]-8-[v1]|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0":ProfileImageView, "v1":NameLabel]))
+        
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[v0]|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0":NameLabel]))
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-8-[v0(44)]|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0":ProfileImageView]))
+        
+    }
+    
+    
+    
+    let  NameLabel: UILabel  = {
+        let label = UILabel()
+        label.text = "This is Simple text"
+        label.textColor = UIColor.black
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    
+    let ProfileImageView : UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        imageView.backgroundColor = UIColor.red
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+        
+    }()
+    
+    
+}
+
+
+extension UIView {
+    func addConstraisWithFormat(format: String, views: UIView...) {
+        var viewDictonary = [String: UIView]()
+        for (index, view) in views.enumerated() {
+            let key = "v\(index)"
+            viewDictonary[key] = view
+            view.translatesAutoresizingMaskIntoConstraints = false
+        }
+        
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: format, options: NSLayoutFormatOptions(), metrics: nil
+        , views: viewDictonary))
+    }
+}
+
+
+
+
+
+
+
+
+
 
